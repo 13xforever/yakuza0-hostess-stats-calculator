6174f83d37de7a6d8e531ee9d2454b83199ff125
13xforever <ilya.veselov@gmail.com>
2019-07-09 00:47:53 +0500

    + Ai

--- Yakuza0DressupMaximizer/Program.cs
+++ Yakuza0DressupMaximizer/Program.cs
@@ -32,17 +32,18 @@ namespace Yakuza0DressupMaximizer
 
             var lookup = new Dictionary<string, Dictionary<string, Item>>
             {
-                ["HAIRACC"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "HAIRACC" } },
-                ["GLASSES"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "GLASSES" } },
-                ["EARRING"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "EARRING" } },
-                ["NECKLACE"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "NECKLACE" } },
-                ["NAIL"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "NAIL" } },
-                ["RING"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "RING" } },
-                ["WATCH"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "WATCH" } },
-                ["BRACELET"] = new Dictionary<string, Item> { ["None"] = new Item{ Name = "None", Kind = "BRACELET" } },
+                ["HAIRACC"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "HAIRACC"}},
+                ["GLASSES"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "GLASSES"}},
+                ["EARRING"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "EARRING"}},
+                ["NECKLACE"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "NECKLACE"}},
+                ["NAIL"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "NAIL"}},
+                ["RING"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "RING"}},
+                ["WATCH"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "WATCH"}},
+                ["BRACELET"] = new Dictionary<string, Item> {["None"] = new Item {Name = "None", Kind = "BRACELET"}},
                 ["HOSTESS"] = new Dictionary<string, Item>
                 {
-                    ["Yuki"] = new Item { Name = "Yuki", Kind = "HOSTESS", Sexy = -1, Beauty = 2, Cuty = 2, Funny = 1},
+                    ["Yuki"] = new Item {Name = "Yuki", Kind = "HOSTESS", Sexy = -1, Beauty = 2, Cuty = 2, Funny = 1},
+                    ["Ai"] = new Item {Name = "Ai", Kind = "HOSTESS", Sexy = 1, Beauty = 1, Cuty = 2, Funny = 2},
                 }
             };
             foreach (var i in dressUpItems.Where(p => p.Key != "types").Select(p => p.Value))
@@ -61,59 +62,64 @@ namespace Yakuza0DressupMaximizer
             }
 
             var hostesses = lookup["HOSTESS"];
-            foreach (var hostess in hostesses.Values)
+            using (var output = File.Open(@".\results.csv", FileMode.Create, FileAccess.Write, FileShare.Read))
+            using (var writer = new StreamWriter(output, new UTF8Encoding(false)))
             {
-                Console.WriteLine($"Calculating outfits for {hostess.Name}...");
-                var outfits = new Dictionary<int, Dictionary<(int s, int b, int c, int f), List<(long cost, Dictionary<string, Item> outfit)>>>();
-                var minOutfitCost = new Dictionary<(int s, int b, int c, int f), (long min, long upper, long stdMin, long stdUpper)>();
-                var dresses = lookup["DRESS"];
-                var hairs = lookup["HAIR"];
-                var hairAccessories = lookup["HAIRACC"];
-                var glasses = lookup["GLASSES"];
-                var earrings = lookup["EARRING"];
-                var necklaces = lookup["NECKLACE"];
-                var nails = lookup["NAIL"];
-                var rings = lookup["RING"];
-                var watches = lookup["WATCH"];
-                var bracelets = lookup["BRACELET"];
-                foreach (var d in dresses.Values)
-                foreach (var h in hairs.Values)
-                foreach (var ha in hairAccessories.Values)
-                foreach (var g in glasses.Values)
-                foreach (var e in earrings.Values)
-                foreach (var nl in necklaces.Values)
-                foreach (var n in nails.Values)
-                foreach (var r in rings.Values)
-                foreach (var w in watches.Values)
-                foreach (var b in bracelets.Values)
+                writer.WriteLine("Hostess;Total Score;Sexy;Beauty;Cute;Funny;Cost;Dress;Hair;Hair Accessory;Glasses;Earring;Necklace;Nail;Ring;Watch;Bracelet");
+                foreach (var hostess in hostesses.Values)
                 {
-                    var s = GetScore( hostess, d, h, ha, g, e, nl, n, r, w, b);
-                    if (s.total < 9)
-                        continue;
+                    Console.WriteLine($"Calculating outfits for {hostess.Name}...");
+                    var outfits = new Dictionary<int, Dictionary<(int s, int b, int c, int f), List<(long cost, Dictionary<string, Item> outfit)>>>();
+                    var minOutfitCost = new Dictionary<(int s, int b, int c, int f), (long min, long upper, long stdMin, long stdUpper)>();
+                    var dresses = lookup["DRESS"];
+                    var hairs = lookup["HAIR"];
+                    var hairAccessories = lookup["HAIRACC"];
+                    var glasses = lookup["GLASSES"];
+                    var earrings = lookup["EARRING"];
+                    var necklaces = lookup["NECKLACE"];
+                    var nails = lookup["NAIL"];
+                    var rings = lookup["RING"];
+                    var watches = lookup["WATCH"];
+                    var bracelets = lookup["BRACELET"];
 
-                    if (!outfits.TryGetValue(s.total, out var outfitsPerScore))
-                        outfits[s.total] = outfitsPerScore = new Dictionary<(int s, int b, int c, int f), List<(long cost, Dictionary<string, Item> outfit)>>();
-
-                    var outfit = new Dictionary<string, Item>
+                    foreach (var d in dresses.Values)
+                    foreach (var h in hairs.Values)
+                    foreach (var ha in hairAccessories.Values)
+                    foreach (var g in glasses.Values)
+                    foreach (var e in earrings.Values)
+                    foreach (var nl in necklaces.Values)
+                    foreach (var n in nails.Values)
+                    foreach (var r in rings.Values)
+                    foreach (var w in watches.Values)
+                    foreach (var b in bracelets.Values)
                     {
-                        ["DRESS"] = d,
-                        ["HAIR"] = h,
-                        ["HAIRACC"] = ha,
-                        ["GLASSES"] = g,
-                        ["EARRING"] = e,
-                        ["NECKLACE"] = nl,
-                        ["NAIL"] = n,
-                        ["RING"] = r,
-                        ["WATCH"] = w,
-                        ["BRACELET"] = b,
-                    };
-                    if (!minOutfitCost.TryGetValue(s.stats, out var minCost))
-                        minOutfitCost[s.stats] = minCost = (long.MaxValue, long.MaxValue, long.MaxValue, long.MaxValue);
-                    if (s.cost > minCost.upper && (d.Name != "Standard" || h.Name != "Standard") || s.cost > minCost.stdUpper)
-                        continue;
+                        var s = GetScore(hostess, d, h, ha, g, e, nl, n, r, w, b);
+                        if (s.total < 9)
+                            continue;
 
-                    if (!outfitsPerScore.TryGetValue(s.stats, out var outfitList))
-                        outfitsPerScore[s.stats] = outfitList = new List<(long cost, Dictionary<string, Item> outfit)>();
+                        if (!outfits.TryGetValue(s.total, out var outfitsPerScore))
+                            outfits[s.total] = outfitsPerScore = new Dictionary<(int s, int b, int c, int f), List<(long cost, Dictionary<string, Item> outfit)>>();
+
+                        var outfit = new Dictionary<string, Item>
+                        {
+                            ["DRESS"] = d,
+                            ["HAIR"] = h,
+                            ["HAIRACC"] = ha,
+                            ["GLASSES"] = g,
+                            ["EARRING"] = e,
+                            ["NECKLACE"] = nl,
+                            ["NAIL"] = n,
+                            ["RING"] = r,
+                            ["WATCH"] = w,
+                            ["BRACELET"] = b,
+                        };
+                        if (!minOutfitCost.TryGetValue(s.stats, out var minCost))
+                            minOutfitCost[s.stats] = minCost = (long.MaxValue, long.MaxValue, long.MaxValue, long.MaxValue);
+                        if (s.cost > minCost.upper && (d.Name != "Standard" || h.Name != "Standard") || s.cost > minCost.stdUpper)
+                            continue;
+
+                        if (!outfitsPerScore.TryGetValue(s.stats, out var outfitList))
+                            outfitsPerScore[s.stats] = outfitList = new List<(long cost, Dictionary<string, Item> outfit)>();
 
 /*
                     if (d.Name == "Standard" && h.Name == "Standard" && s.cost < minCost.stdUpper)
@@ -126,31 +132,26 @@ namespace Yakuza0DressupMaximizer
                     }
 */
 
-                    if (s.cost < minCost.min)
-                    {
-                        var newMin = s.cost;
-                        var newUpper = (long)(s.cost * 2);
-                        minOutfitCost[s.stats] = minCost = (newMin, newUpper, minCost.stdMin, minCost.stdUpper);
-                        var newOutfitList = outfitList.Where(o => o.cost < minCost.upper || (o.outfit["DRESS"].Name == "Standard" && o.outfit["HAIR"].Name == "Standard" && o.cost < minCost.stdUpper)).ToList();
-                        outfitsPerScore[s.stats] = outfitList = newOutfitList;
+                        if (s.cost < minCost.min)
+                        {
+                            var newMin = s.cost;
+                            var newUpper = (long)(s.cost * 2);
+                            minOutfitCost[s.stats] = minCost = (newMin, newUpper, minCost.stdMin, minCost.stdUpper);
+                            var newOutfitList = outfitList.Where(o => o.cost < minCost.upper || (o.outfit["DRESS"].Name == "Standard" && o.outfit["HAIR"].Name == "Standard" && o.cost < minCost.stdUpper)).ToList();
+                            outfitsPerScore[s.stats] = outfitList = newOutfitList;
+                        }
+
+                        outfitList.Add((s.cost, outfit));
                     }
 
-                    outfitList.Add((s.cost, outfit));
-                }
-
-                Console.WriteLine($"Best score: {outfits.Keys.Max()}, time: {stopwatch.Elapsed:g}");
-                using (var output = File.Open(@".\results.csv", FileMode.Create, FileAccess.Write, FileShare.Read))
-                using (var writer = new StreamWriter(output, new UTF8Encoding(false)))
-                {
-                    writer.WriteLine("Hostess;Total Score;Sexy;Beauty;Cute;Funny;Cost;Dress;Hair;Hair Accessory;Glasses;Earring;Necklace;Nail;Ring;Watch;Bracelet");
+                    Console.WriteLine($"Best score: {outfits.Keys.Max()}, time: {stopwatch.Elapsed:g}");
                     foreach (var ol in outfits.OrderByDescending(i => i.Key))
                     foreach (var oc in ol.Value)
                     foreach (var os in oc.Value.OrderBy(i => i.cost))
                     {
                         var s = oc.Key;
                         var o = os.outfit;
-                        writer.WriteLine(
-                            $"{hostess.Name};{ol.Key};{s.s};{s.b};{s.c};{s.f};{os.cost};{o["DRESS"].Name};{o["HAIR"].Name};{o["HAIRACC"].Name};{o["GLASSES"].Name};{o["EARRING"].Name};{o["NECKLACE"].Name};{o["NAIL"].Name};{o["RING"].Name};{o["WATCH"].Name};{o["BRACELET"].Name}");
+                        writer.WriteLine($"{hostess.Name};{ol.Key};{s.s};{s.b};{s.c};{s.f};{os.cost};{o["DRESS"].Name};{o["HAIR"].Name};{o["HAIRACC"].Name};{o["GLASSES"].Name};{o["EARRING"].Name};{o["NECKLACE"].Name};{o["NAIL"].Name};{o["RING"].Name};{o["WATCH"].Name};{o["BRACELET"].Name}");
                     }
                 }
             }
